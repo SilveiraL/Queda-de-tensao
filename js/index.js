31ab1e34da918f5104194ce54c9b1f5477e22ad2
Silveira_L <luan.w.silveira@gmail.com>
2019-06-05 23:14:59 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,30 +3,83 @@ const $ = require('jquery');
 var objetoEdicao;
 
 class Componente {
-    constructor(nome_componente) {
-        this.componente = $('<div>')
-            .addClass(nome_componente)
+    constructor() {
+        this.dom = $('<div>')
             .addClass('componente')
             .addClass('bg-primary')
+            .addClass('seguir-mouse')
             .css('position', 'absolute')
 
-        $('main').append(this.componente);
+        const main = $('main');
+
+        main.append(this.dom).mousemove(e => {
+            if (this.dom.hasClass('seguir-mouse')) {
+                let x = e.clientX;
+                let y = e.clientY;
+
+                x -= parseInt(this.dom.css('width').replace('px', '')) / 2;
+                y -= parseInt(this.dom.css('height').replace('px', '')) / 2;
+
+                this.dom.css('left', x).css('top', y);
+            }
+        });
+
+        main.click(e => {
+            if (this.dom.hasClass('seguir-mouse')) {
+                this.dom.removeClass('seguir-mouse');
+                this.setX(0);
+                this.setY(0);
+                console.log()
+            }
+        });
+    }
+
+    setX(x) {
+        this.dom.css('left', x);
+    }
+
+    setY(y) {
+        this.dom.css('top', y);
+    }
+
+    getX() {
+        return parseInt(this.dom.css('left').replace('px', ''));
+    }
+
+    getY() {
+        return parseInt(this.dom.css('bottom').replace('px', ''));
+    }
+}
+
+class Transformador extends Componente {
+    constructor() {
+        super();
+
+        this.dom.addClass('transformador');
+    }
+}
+
+class No extends Componente {
+    constructor() {
+        super();
+
+        this.dom.addClass('no');
     }
 }
 
 $('.componente').click(e => {
-    objetoEdicao = new Componente(e.target.id);
-});
+    switch (e.target.id) {
+        case 'transformador':
+            objetoEdicao = new Transformador();
+            break;
 
-$('main').mousemove(e => {
-    if (!!objetoEdicao) {
-        let x = e.clientX;
-        let y = e.clientY;
-
-        x -= parseInt(objetoEdicao.componente.css('width').replace('px', '')) / 2;
-        y -= parseInt(objetoEdicao.componente.css('height').replace('px', '')) / 2;
-
-        objetoEdicao.componente.css('left', x).css('top', y);
-        console.log(x, y);
+        case 'no':
+            objetoEdicao = new No();
     }
 });
+
+// $('main').mousemove(e => {
+//     if (!!objetoEdicao) {
+
+//     }
+// });
